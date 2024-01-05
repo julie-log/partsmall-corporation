ac9cff6d5234e4418c39e0b6f55565bc277c05db
julie-log <58288221+julie-log@users.noreply.github.com>
2024-01-05 12:07:22 +0900

    menu 수정

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,19 @@
+const fullMenu = document.getElementById("full-menu");
 $(function () {
   $(".depth1 > li").hover(
     function () {
       // $(this).find(".depth2").addClass("active");
       $(".depth2").addClass("active");
+      $(".side-menu").addClass("active");
     },
     function () {
       // $(this).find(".depth2").removeClass("active");
       $(".depth2").removeClass("active");
+      $(".side-menu").removeClass("active");
     }
   );
 });
+
+fullMenu.addEventListener("click",function(){
+  $(".depth2").toggleClass("active");
+})
